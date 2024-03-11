b26b40240456c53f94db3b3045a68ddc549c4b77
Victor G <vesbru@yahoo.fr>
2024-03-11 22:27:58 +0100

    merge successful, test level switch

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -57,7 +57,7 @@ func __get_load_game_item(save_metadata:SaveInfoItem)->HBoxContainer:
 	return ret
 
 func _create_new_game():
-	pass
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
 	
 func _load_and_start_game(save_path:String):
 	pass
