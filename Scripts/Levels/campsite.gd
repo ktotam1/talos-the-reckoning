854da871656c7056687c2379cfdd30284668d3a9
alex <anithacan01@gmail.com>
2024-04-09 19:13:05 +0200

    campsite

--- Scripts/Levels/campsite.gd
+++ Scripts/Levels/campsite.gd
@@ -1,15 +1,25 @@
 extends Node
 
-@onready var forest_border: RigidBody2D = $Trees/Border
-@onready var forest_text: RichTextLabel = $Trees/PathBlockedMessage
-
+@onready var chest: AnimatedSprite2D = $Chest/AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
-	forest_text.visible = false # Replace with function bod # Replace with function body.
+	chest.play("closed") # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
+
+func _on_area_2d_body_entered(body):
+	if body is CharacterBody2D:
+		chest.play("opening")
+
+func _on_area_2d_body_exited(body):
+	chest.play("closing")
+
+
+func _on_animated_sprite_2d_animation_finished(anim_name):
+	if anim_name == "closing":
+		chest.play("closed") # Replace with function body.
+	if anim_name == "opening":
+		chest.play("opened")
