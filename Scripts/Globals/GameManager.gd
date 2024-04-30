e0bbaa8a772f4035de321d527f512a8e51fd3686
alex <anithacan01@gmail.com>
2024-04-30 16:40:18 +0200

    hi

--- Scripts/Globals/GameManager.gd
+++ Scripts/Globals/GameManager.gd
@@ -55,6 +55,6 @@ func set_current_level(scene_name : String):
 		current_level.queue_free()
 		
 	var new_level_scene_res = load(scene_name)
-	var new_level_scene_node = new_level_scene_res.instantiate()
+d	var new_level_scene_node = new_level_scene_res.instantiate()
 	current_level_parent.add_child(new_level_scene_node)
 
