69c70c3bec33acf12df059a95ed9ce68ba37f3dd
alex <anithacan01@gmail.com>
2024-04-30 15:03:06 +0200

    haahhahahahahaahahahahaha ah aha ha a a ha ha ha a h ah h fucking dies

--- Scripts/Characters/player.gd
+++ Scripts/Characters/player.gd
@@ -6,15 +6,21 @@ extends CharacterBody2D
 @onready var pistol_bullet_marker : Marker2D = $Hand/Pivot/Pistol/PistolBulletMarker
 @onready var animator : AnimatedSprite2D = $AnimatedSprite2D
 @onready var hand : Node2D = $Hand
+@onready var healthbar = $ProgressBar
 
+var MAX_HEALTH = 4
+var health = MAX_HEALTH
+var dying = false
+var elapsed_death = 0.0
 
+@export var knockback_strength = 18
+var knockback = Vector2(0,0)
 var charging_jump = false
 var jump_held = Time.get_ticks_msec()
 var did_double_jump = false
 func input_movement():
 	var horizontal_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.x = horizontal_input * speed
-	
+	velocity.x = horizontal_input * speed + knockback.x * knockback_strength
 	if is_on_floor():
 		did_double_jump = false
 	else: 
@@ -31,7 +37,8 @@ func input_movement():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
+	healthbar.max_value = MAX_HEALTH
+	healthbar.value = health
 	pass # Replace with function body.
 
 
@@ -78,14 +85,38 @@ func animate(input_vector):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	if elapsed_death > 1:
+		get_tree().change_scene_to_file("res://Scenes/Levels/campsite.tscn")# Replace with function body.
+		return
+	if health <= 0:
+		dying = true
+	if dying:
+		animator.rotate(-0.025)
+		animator.translate(Vector2(-.4,.1))
+		elapsed_death += delta
+		return
 	velocity.y = min(velocity.y + gravity * delta, 200)
 	input_movement()
 	#player_animations()
 	var input_vector = Input.get_axis("ui_left","ui_right")
 	animate(input_vector)
+
+	velocity += knockback * knockback_strength
+	knockback = lerp(knockback, Vector2.ZERO, 0.1)
+	
+
 	move_and_slide() 
 
 
 
 func _on_border_body_entered(body):
 	pass # Replace with function body.
+
+
+func _on_hurtbox_area_entered(area):
+	health -= 1
+	healthbar.value = health
+	knockback = global_position - area.global_position
+	knockback.y = -.8
+	knockback.x = sign(knockback.x)*40
+	print(knockback * knockback_strength)
